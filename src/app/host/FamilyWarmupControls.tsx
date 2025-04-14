f389b7efce3639f242c11913b92c9cf10aea0fe2
pgrobban <robert.sebescen@surikat.se>
2025-04-14 08:19:27 +0200

    fixed lint issues

--- src/app/host/FamilyWarmupControls.tsx
+++ src/app/host/FamilyWarmupControls.tsx
@@ -9,44 +9,44 @@ import AwardPointsButton from "./AwardPointsButton";
 import FamilyWarmupQuestionInProgressControls from "./FamilyWarmupQuestionInProgressControls";
 
 export default function FamilyWarmupControls({
-  gameState,
+	gameState,
 }: {
-  gameState: GameState & FamilyWarmUpGame;
+	gameState: GameState & FamilyWarmUpGame;
 }) {
-  const socket = useSocket();
+	const socket = useSocket();
 
-  if (
-    gameState?.status !== "in_progress" ||
-    gameState?.mode !== "family_warm_up"
-  ) {
-    return null;
-  }
+	if (
+		gameState?.status !== "in_progress" ||
+		gameState?.mode !== "family_warm_up"
+	) {
+		return null;
+	}
 
-  const requestRevealTeamAnswers = () =>
-    socket?.emit("requestRevealTeamAnswers");
+	const requestRevealTeamAnswers = () =>
+		socket?.emit("familyWarmup:requestRevealTeamAnswers");
 
-  switch (gameState.modeStatus) {
-    case "waiting_for_question":
-      return <QuestionPicker />;
-    case "question_in_progress":
-      return <FamilyWarmupQuestionInProgressControls />;
-    case "gathering_team_answers":
-      return (
-        <TeamAnswerSelector storedAnswers={gameState.question?.answers || []} />
-      );
-    case "revealing_stored_answers":
-      return (
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={requestRevealTeamAnswers}
-        >
-          Reveal team answers
-        </Button>
-      );
-    case "revealing_team_answers":
-      return <AwardPointsButton />;
-    case "awarding_points":
-      return <QuestionOverControls />;
-  }
+	switch (gameState.modeStatus) {
+		case "waiting_for_question":
+			return <QuestionPicker />;
+		case "question_in_progress":
+			return <FamilyWarmupQuestionInProgressControls />;
+		case "gathering_team_answers":
+			return (
+				<TeamAnswerSelector storedAnswers={gameState.question?.answers || []} />
+			);
+		case "revealing_stored_answers":
+			return (
+				<Button
+					variant="contained"
+					color="primary"
+					onClick={requestRevealTeamAnswers}
+				>
+					Reveal team answers
+				</Button>
+			);
+		case "revealing_team_answers":
+			return <AwardPointsButton currentMode={gameState.mode} />;
+		case "awarding_points":
+			return <QuestionOverControls />;
+	}
 }
--- src/app/host/FastMoneyControls.tsx
+++ src/app/host/FastMoneyControls.tsx
@@ -1,27 +1,24 @@
-import useSocket from "@/hooks/useSocket";
-import { FastMoneyGame, GameState } from "@/shared/types";
+import type { FastMoneyGame, GameState } from "@/shared/types";
 import FastMoneyQuestionsPicker from "./FastMoneyQuestionsPicker";
 import FastMoneyAnswersPicker from "./FastMoneyQuestionInProgressControls";
 
 export default function FastMoneyControls({
-  gameState,
+	gameState,
 }: {
-  gameState: GameState & FastMoneyGame;
+	gameState: GameState & FastMoneyGame;
 }) {
-  const socket = useSocket();
+	if (gameState?.status !== "in_progress" || gameState?.mode !== "fast_money") {
+		return null;
+	}
 
-  if (gameState?.status !== "in_progress" || gameState?.mode !== "fast_money") {
-    return null;
-  }
+	console.log("***", gameState);
 
-  console.log("***", gameState);
-
-  switch (gameState.modeStatus) {
-    case "waiting_for_questions":
-      return <FastMoneyQuestionsPicker />;
-    case "questions_in_progress":
-      return <FastMoneyAnswersPicker gameState={gameState} />;
-    default:
-      return null;
-  }
+	switch (gameState.modeStatus) {
+		case "waiting_for_questions":
+			return <FastMoneyQuestionsPicker />;
+		case "questions_in_progress":
+			return <FastMoneyAnswersPicker gameState={gameState} />;
+		default:
+			return null;
+	}
 }
