f1ddfa1115198c9cd570506a77e9ec435e3b13dd
Robert Sebescen <pgrobban@gmail.com>
2025-04-20 20:16:20 +0200

    added complete test for team 2 doing the fast money round

--- __tests__/CompleteGameTests.ts
+++ __tests__/CompleteGameTests.ts
@@ -1,17 +1,54 @@
 import Game from "@/server/controllers/Game";
-import { createMockSocketServerAndRoom } from "./helpers/testHelpers";
 import {
-  familyWarmupScenarios,
-  runFamilyWarmupScenario,
-} from "./helpers/familyWarmupFixtures";
-import {
-  faceOffScenarios,
-  runFaceOffScenario,
-} from "./helpers/faceOffFixtures";
-import {
-  fastMoneyScenarios,
-  runFastMoneyScenario,
-} from "./helpers/fastMoneyFixtures";
+  createMockSocketServerAndRoom,
+  FaceOffScenario,
+  FamilyWarmupScenario,
+  FastMoneyScenario,
+} from "./helpers/testHelpers";
+import { runFamilyWarmupScenario } from "./helpers/familyWarmupFixtures";
+import { question, runFaceOffScenario } from "./helpers/faceOffFixtures";
+import { runFastMoneyScenario } from "./helpers/fastMoneyFixtures";
+import { FAST_MONEY_QUESTIONS } from "@/shared/utils";
+
+const familyWarmupScenario: FamilyWarmupScenario = {
+  name: "Team 1 gets more points",
+  team1Answers: ["apple", "cherry", "strawberry"],
+  team2Answers: ["cherry", "raspberry"],
+  expectedPoints: [90, 40],
+};
+const faceOffTeam1WinsScenario: FaceOffScenario = {
+  name: "Team 1 buzzes first, highest answer, chooses to play, clears board",
+  buzzedFirst: 1,
+  firstAnswer: "apple",
+  controlChoice: "play",
+  controlGuesses: ["cherry", "strawberry", "raspberry"],
+  expectedPoints: [100, 0],
+};
+const faceOffTeam2WinsScenario: FaceOffScenario = {
+  name: "Team 2 buzzes first, highest answer, chooses to play, clears board",
+  buzzedFirst: 2,
+  firstAnswer: "apple",
+  controlChoice: "play",
+  controlGuesses: ["cherry", "strawberry", "raspberry"],
+  expectedPoints: [0, 100],
+};
+
+const fastMoneyTeam1PlaysScenario: FastMoneyScenario = {
+  name: "team 1 plays, gets over 150 points",
+  questionTexts: new Array(FAST_MONEY_QUESTIONS).fill(question.questionText),
+  answerTexts: new Array(FAST_MONEY_QUESTIONS).fill(
+    question.answers[0].answerText
+  ),
+  expectedPoints: [200, 0],
+};
+const fastMoneyTeam2PlaysScenario: FastMoneyScenario = {
+  name: "team 2 plays, gets over 150 points",
+  questionTexts: new Array(FAST_MONEY_QUESTIONS).fill(question.questionText),
+  answerTexts: new Array(FAST_MONEY_QUESTIONS).fill(
+    question.answers[0].answerText
+  ),
+  expectedPoints: [0, 200],
+};
 
 describe("Full game tests", () => {
   let game: Game;
@@ -28,30 +65,71 @@ describe("Full game tests", () => {
     jest.useRealTimers();
   });
 
-  test("Team 1, wins family warm-up, face off, fast money", () => {
+  test("Team 1 wins family warm-up, face off, fast money", () => {
     game.joinHost("testRoom");
     game.hostPickedMode("family_warm_up");
-    let result = runFamilyWarmupScenario(game, familyWarmupScenarios[0], true);
-    let expectedPointsSoFar = familyWarmupScenarios[0].expectedPoints;
+    let result = runFamilyWarmupScenario(game, familyWarmupScenario, true);
+    let expectedPointsSoFar = familyWarmupScenario.expectedPoints;
     expect(result.teamsAndPoints[0].points).toBe(expectedPointsSoFar[0]);
     expect(result.teamsAndPoints[1].points).toBe(expectedPointsSoFar[1]);
 
     game.cancelQuestionOrMode();
     game.hostPickedMode("face_off");
-    result = runFaceOffScenario(game, faceOffScenarios[0], true);
+    result = runFaceOffScenario(game, faceOffTeam1WinsScenario, true);
     expectedPointsSoFar = [
-      expectedPointsSoFar[0] + faceOffScenarios[0].expectedPoints[0],
-      expectedPointsSoFar[1] + faceOffScenarios[0].expectedPoints[1],
+      expectedPointsSoFar[0] + faceOffTeam1WinsScenario.expectedPoints[0],
+      expectedPointsSoFar[1] + faceOffTeam1WinsScenario.expectedPoints[1],
     ];
     expect(result.teamsAndPoints[0].points).toBe(expectedPointsSoFar[0]);
     expect(result.teamsAndPoints[1].points).toBe(expectedPointsSoFar[1]);
 
     game.cancelQuestionOrMode();
     game.hostPickedMode("fast_money");
-    result = runFastMoneyScenario(game, fastMoneyScenarios[0], true);
+    result = runFastMoneyScenario(game, fastMoneyTeam1PlaysScenario, true);
     expectedPointsSoFar = [
-      expectedPointsSoFar[0] + fastMoneyScenarios[0].expectedPoints[0],
-      expectedPointsSoFar[1] + fastMoneyScenarios[0].expectedPoints[1],
+      expectedPointsSoFar[0] + fastMoneyTeam1PlaysScenario.expectedPoints[0],
+      expectedPointsSoFar[1] + fastMoneyTeam1PlaysScenario.expectedPoints[1],
+    ];
+    expect(result.teamsAndPoints[0].points).toBe(expectedPointsSoFar[0]);
+    expect(result.teamsAndPoints[1].points).toBe(expectedPointsSoFar[1]);
+  });
+
+  test("Team 1, wins family warm-up, team 2 face off (2 questions) and fast money", () => {
+    game.joinHost("testRoom");
+    game.hostPickedMode("family_warm_up");
+
+    let result = runFamilyWarmupScenario(game, familyWarmupScenario, true);
+    let expectedPointsSoFar = familyWarmupScenario.expectedPoints;
+    expect(result.teamsAndPoints[0].points).toBe(expectedPointsSoFar[0]);
+    expect(result.teamsAndPoints[1].points).toBe(expectedPointsSoFar[1]);
+
+    game.cancelQuestionOrMode();
+    game.hostPickedMode("face_off");
+
+    result = runFaceOffScenario(game, faceOffTeam2WinsScenario, true);
+    expectedPointsSoFar = [
+      expectedPointsSoFar[0] + faceOffTeam2WinsScenario.expectedPoints[0],
+      expectedPointsSoFar[1] + faceOffTeam2WinsScenario.expectedPoints[1],
+    ];
+    expect(result.teamsAndPoints[0].points).toBe(expectedPointsSoFar[0]);
+    expect(result.teamsAndPoints[1].points).toBe(expectedPointsSoFar[1]);
+    game.cancelQuestionOrMode();
+
+    result = runFaceOffScenario(game, faceOffTeam2WinsScenario, true);
+    expectedPointsSoFar = [
+      expectedPointsSoFar[0] + faceOffTeam2WinsScenario.expectedPoints[0],
+      expectedPointsSoFar[1] + faceOffTeam2WinsScenario.expectedPoints[1],
+    ];
+    expect(result.teamsAndPoints[0].points).toBe(expectedPointsSoFar[0]);
+    expect(result.teamsAndPoints[1].points).toBe(expectedPointsSoFar[1]);
+
+    game.cancelQuestionOrMode();
+    game.hostPickedMode("fast_money");
+
+    result = runFastMoneyScenario(game, fastMoneyTeam2PlaysScenario, true);
+    expectedPointsSoFar = [
+      expectedPointsSoFar[0] + fastMoneyTeam2PlaysScenario.expectedPoints[0],
+      expectedPointsSoFar[1] + fastMoneyTeam2PlaysScenario.expectedPoints[1],
     ];
     expect(result.teamsAndPoints[0].points).toBe(expectedPointsSoFar[0]);
     expect(result.teamsAndPoints[1].points).toBe(expectedPointsSoFar[1]);
